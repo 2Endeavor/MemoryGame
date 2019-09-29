54b8eeb8bb83d43f6b39d59f019e63d45e78a9e2
Maryanne Heinbaugh <23heinbau@gmail.com>
2019-09-28 17:16:29 -0700

    Updates

---
+++ precache-manifest.ed47429cca904f67da1b1b1e58258af3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5acbf47c0add57db21179f367d2855d9",
+    "url": "/memorygame/index.html"
+  },
+  {
+    "revision": "fff7cb62b2cbd27b4a6a",
+    "url": "/memorygame/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ec0b1a0b2d8cc2a75fc5",
+    "url": "/memorygame/static/js/2.c1e9b3a1.chunk.js"
+  },
+  {
+    "revision": "fff7cb62b2cbd27b4a6a",
+    "url": "/memorygame/static/js/main.28420909.chunk.js"
+  },
+  {
+    "revision": "2bb9b6dbd1430ea554bd",
+    "url": "/memorygame/static/js/runtime-main.34f704a5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/memorygame/static/media/logo.25bf045c.svg"
+  }
+]);
