7b8a70aad51c70f67a6af247f5738bb2d5190af0
Maryanne Heinbaugh <23heinbau@gmail.com>
2019-09-29 18:05:08 -0700

    added title component

---
+++ src/components/Title/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+
+
+function Title(){
+  return(
+    <div>
+
+
+    <h1>Test your Memory!</h1>
+    <h3>Don't click on the same photo more than once</h3>
+    </div>
+    )
+  
+}
+
+
+export default Title;
